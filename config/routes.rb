3a7b5807a9393b3cdc34e7cc384a1a43d2c21210
Edward Ocampo-Gooding <edward@edwardog.net>
2013-08-27 20:08:56 -0400

    Routes all GET requests to batman

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Forage::Application.routes.draw do
-  resources :places
-
   get "(*redirect_path)", to: "batman#index", constraints: lambda { |request| request.format == "text/html" }
+
+  resources :places
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
