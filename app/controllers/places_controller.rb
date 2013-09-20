07fc04948e569ae472301dcd9c97b13317bcb06d
Kevin McPhillips <github@kevinmcphillips.ca>
2013-09-19 22:41:04 -0400

    Filter by tags

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,28 +1,23 @@
 class PlacesController < ApplicationController
   before_action :set_place, only: [:show, :edit, :update, :destroy]
 
-  # GET /places
-  # GET /places.json
   def index
     @places = Place.all
+    @places = @places.tagged_with(params[:tag]) if params[:tag]
+
+    @tags = Place.tag_counts_on(:tags)
   end
 
-  # GET /places/1
-  # GET /places/1.json
   def show
   end
 
-  # GET /places/new
   def new
     @place = Place.new
   end
 
-  # GET /places/1/edit
   def edit
   end
 
-  # POST /places
-  # POST /places.json
   def create
     @place = Place.new(place_params)
 
@@ -37,8 +32,6 @@ class PlacesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /places/1
-  # PATCH/PUT /places/1.json
   def update
     respond_to do |format|
       if @place.update(place_params)
@@ -51,8 +44,6 @@ class PlacesController < ApplicationController
     end
   end
 
-  # DELETE /places/1
-  # DELETE /places/1.json
   def destroy
     @place.destroy
     respond_to do |format|
@@ -62,13 +53,13 @@ class PlacesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_place
-      @place = Place.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def place_params
-      params.require(:place).permit(:name, :url, :phone_number, :address, :description, :walking_time_in_minutes)
-    end
+  def set_place
+    @place = Place.find(params[:id])
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :url, :phone_number, :address, :description, :walking_time_in_minutes)
+  end
+
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
+  RESERVED_TAGS = ["eat it", "drink it", "do it"]
+
   acts_as_taggable
 
   validate :name, :address, presence: true
