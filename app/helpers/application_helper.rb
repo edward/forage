3ac40cf172b68fe4b5d164ad79222585c8de2e41
Kevin McPhillips <github@kevinmcphillips.ca>
2013-09-19 23:31:08 -0400

    Add active classes for tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,22 @@ module ApplicationHelper
     end
   end
 
+  def tag_cloud_list(tags)
+    list = content_tag(:li, link_to("All", places_path(distance: params[:distance], category: params[:category]), class: (params[:tag].present? ? '' : 'active')))
 
+    tag_cloud(@tags, []) do |tag, css_class|
+      unless Place::RESERVED_TAGS.include?(tag.name)
+        link = link_to(
+          tag.name.humanize, 
+          places_path(tag: tag.name, distance: params[:distance], category: params[:category]),
+          class: (params[:tag] == tag.name ? 'active' : '')
+        )
+        list << content_tag(:li, link)
+      end
+    end
+
+    list.html_safe
+
+  end
 
 end
