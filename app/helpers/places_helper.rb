26e4490d97d6603023be1b69c65985e30f8ada77
Edward Ocampo-Gooding <edward@edwardog.net>
2013-09-20 13:17:59 -0400

    Adds neato google maps to the show pages

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,2 +1,9 @@
 module PlacesHelper
+  def link_to_directions_map(destination_address, options = nil, html_options = nil, &block)
+    if block
+      link_to "https://www.google.ca/maps/?saddr=#{CGI.escape(Place::SHOPIFY_OTTAWA_ADDRESS)}&daddr=#{CGI.escape(@place.address)}&dirflg=w", &block
+    else
+      link_to destination_address, "https://www.google.ca/maps/?saddr=#{CGI.escape(Place::SHOPIFY_OTTAWA_ADDRESS)}&daddr=#{CGI.escape(@place.address)}&dirflg=w"
+    end
+  end
 end
