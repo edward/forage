3b94946c0e02353b836c72fa04474e1cd1d451e2
Kevin McPhillips <github@kevinmcphillips.ca>
2013-09-19 23:14:40 -0400

    Add nav tags with 'active'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def nav_it_tag(category)
+    content_tag :li, class: "nav-item #{ params[:category] == category ? 'active' : '' }" do
+      link_to(category.humanize, places_path(tag: params[:tag], distance: params[:distance], category: category))
+    end
+  end
 end
